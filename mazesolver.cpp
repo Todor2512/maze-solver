aaf3e9ba8252dde2b079490d6cd3568299f24f8b
Todor2512 <40757636+Todor2512@users.noreply.github.com>
2018-07-02 11:44:45 +0200

    Create mazesolver.cpp

---
+++ mazesolver.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std; 
+int Mazewidth;
+int Mazeheight;
+cin Mazewidth ; 
+
+char Maze [Mazeheight][Mazewidth] {
+    "# #######",
+    "#   #   #",
+    "# ### # #",
+    "# #   # #",
+    "# # # ###",
+    "#   # # #",
+    "# ### # #",
+    "#   #   #",
+    "####### #",
+
+};
+const char Wall = "#";
+const char Free = " ";
+const char Mazerunner = "*" ; 
